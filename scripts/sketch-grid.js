7fa3c50df028c7363a6ec7239deed3e3c88b7280
CopyypoC <william.wang650@gmail.com>
2024-04-09 17:15:01 -0700

    Add button to create new grid
    New grid button removes the old grid and prompts the user for a width and height.
    Range is from 1 to 100 inclusive, otherwise defaults to 16.

--- scripts/sketch-grid.js
+++ scripts/sketch-grid.js
@@ -1,12 +1,13 @@
 const container = document.querySelector('#container');
+const newGrid = document.querySelector('#new-grid');
 // The number of modules (boxes) width and height has. Actual size of each
 // module is defined in the styles.
 let gridWidthModules = 16;
 let gridHeightModules = 16;
-let gridModulesTotal = gridWidthModules * gridHeightModules;
 
 // Fill container with gridModulesTotal amount of modules as divs
-function createGrid() {
+function createGrid(gridWidthModules, gridHeightModules) {
+    let gridModulesTotal = gridWidthModules * gridHeightModules;
     let gridRow = document.createElement('div');
     for (let  i = 0; i < gridModulesTotal; i++) {
         let gridModule = document.createElement('div');
@@ -14,6 +15,7 @@ function createGrid() {
         if ((i % gridWidthModules === 0) && (i !== 0)) {
             gridRow = document.createElement('div');
             gridRow.setAttribute('class', 'row');
+        // First row is initialized so can't reassign to a new element yet
         } else if ((i % gridWidthModules === 0) && (i === 0)) {
             gridRow.setAttribute('class', 'row');
         }
@@ -21,7 +23,6 @@ function createGrid() {
         gridRow.appendChild(gridModule);
         container.appendChild(gridRow);
     }
-
     // Change module color on hover
     container.addEventListener('mouseover', (e) => {
         let target = e.target;
@@ -31,4 +32,27 @@ function createGrid() {
     })
 }
 
-createGrid();
+function removeAllChildren(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
+
+// Make button reset grid on click and prompt for new grid dimensions
+newGrid.addEventListener('click', () => {
+    // Reset the container
+    removeAllChildren(container);
+    // Prompt user for new width and height
+    gridWidthModules = prompt(`New grid width: (1 - 100 otherwise will default to 16)`);
+    gridHeightModulesModules = prompt(`New grid height: (1 - 100 otherwise will default to 16)`);
+    if (!((gridWidthModules >= 1) && (gridWidthModules <= 100))) {
+        gridWidthModules = 16;
+    }
+    if (!((gridHeightModules >= 1) && (gridHeightModules <= 100))) {
+        gridHeightModules = 16;
+    }    
+    // Create grid with user input
+    createGrid(gridWidthModules, gridHeightModules);
+});
+
+createGrid(gridWidthModules, gridWidthModules);
