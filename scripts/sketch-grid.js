a891036badda30da91843064ffd9e8c2a14c0165
CopyypoC <william.wang650@gmail.com>
2024-04-09 19:20:21 -0700

    Change newGrid size prompt

--- scripts/sketch-grid.js
+++ scripts/sketch-grid.js
@@ -2,8 +2,7 @@ const container = document.querySelector('#container');
 const newGrid = document.querySelector('#new-grid');
 // The number of modules (boxes) width and height has. Actual size of each
 // module is defined in the styles. Default: 16
-let gridWidthModules = 16;
-let gridHeightModules = 16;
+let gridSize = 16;
 
 // Event listener callback to change module color on hover
 function colorModule(event) {
@@ -14,17 +13,16 @@ function colorModule(event) {
 }
 
 // Fill container with total amount of modules as divs
-function createGrid(gridWidthModules, gridHeightModules, hasHoverEvent = false) {
-    let gridModulesTotal = gridWidthModules * gridHeightModules;
+function createGrid(gridSize, hasHoverEvent = false) {
     let gridRow = document.createElement('div');
-    for (let  i = 0; i < gridModulesTotal; i++) {
+    for (let  i = 0; i < gridSize * gridSize; i++) {
         let gridModule = document.createElement('div');
         // Make a row container that holds width amount of columns
-        if ((i % gridWidthModules === 0) && (i !== 0)) {
+        if ((i % gridSize === 0) && (i !== 0)) {
             gridRow = document.createElement('div');
             gridRow.setAttribute('class', 'row');
         // First row is already initialized so can't reassign to a new element yet
-        } else if ((i % gridWidthModules === 0) && (i === 0)) {
+        } else if ((i % gridSize === 0) && (i === 0)) {
             gridRow.setAttribute('class', 'row');
         }
         gridModule.setAttribute('class', 'column');
@@ -48,18 +46,17 @@ newGrid.addEventListener('click', () => {
     let hasHoverEvent = true;
     // Reset the container
     removeAllChildren(container);
-    // Prompt user for new width and height
-    gridWidthModules = Number(prompt(`New grid width: (1 - 100 otherwise will default to 16)`));
-    gridHeightModules = Number(prompt(`New grid height: (1 - 100 otherwise will default to 16)`));
-    if (!((gridWidthModules >= 1) && (gridWidthModules <= 100))) {
-        gridWidthModules = 16;
+    // Prompt user for new size
+    gridSize = Number(prompt(`New grid size: (1 - 100 otherwise will default to 16)`));   
+    if (!((gridSize >= 1) && (gridSize <= 100))) {
+        gridSize = 16;
     }
-    if (!((gridHeightModules >= 1) && (gridHeightModules <= 100))) {
-        gridHeightModules = 16;
+    if (!((gridSize >= 1) && (gridSize <= 100))) {
+        gridSize = 16;
     }    
     // Create new grid with user input
-    createGrid(gridWidthModules, gridHeightModules, hasHoverEvent);
+    createGrid(gridSize, hasHoverEvent);
 });
 
 // Initial grid of 16x16
-createGrid(gridWidthModules, gridWidthModules);
+createGrid(gridSize);
